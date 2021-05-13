2797ae820a4603367f56084ca4495852ac1fdf0f
Yunfei Jing <yunfei.jing@outlook.com>
2021-05-13 11:57:24 +1000

    update result

--- eval.py
+++ eval.py
@@ -7,7 +7,7 @@ from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import metrics
+from sklearn import metrics, preprocessing
 from tqdm import tqdm
 import multiprocessing
 import pandas as pd
@@ -107,69 +107,168 @@ all_tokens = np.concatenate((train_tokens, dev_tokens, test_tokens))
 
 cores = multiprocessing.cpu_count()
 
-for n in [100, 200, 300]:
-    w2v_model = gensim.models.word2vec.Word2Vec(
-        all_tokens, vector_size=n, window=8, min_count=1, sg=1, epochs=30, workers=cores)
-    w2v_words = w2v_model.wv.index_to_key
+w2v_model = gensim.models.word2vec.Word2Vec(
+    all_tokens, vector_size=300, window=8, min_count=1, sg=1, epochs=30, workers=cores)
+w2v_words = w2v_model.wv.index_to_key
 
-    # average Word2Vec
-    # compute average word2vec for each tweet
-    train_vectors = []  # the avg-w2v for each sentence/review is stored in this list
-    for sent in tqdm(train_tokens):  # for each tweet
-        sent_vec = np.zeros(n)  # as word vectors are of zero length 300,
-        cnt_words = 0  # num of words with a valid vector in the sentence/review
-        for word in sent:  # for each word in a tweet
-            if word in w2v_words:
-                vec = w2v_model.wv[word]
-                sent_vec += vec
-                cnt_words += 1
-        if cnt_words != 0:
-            sent_vec /= cnt_words
-        train_vectors.append(sent_vec)
+classifiers = [
+    KNeighborsClassifier(n_neighbors=49),
+    LogisticRegression(n_jobs=-1),
+    LinearSVC(C=0.12, random_state=42),
+    MLPClassifier(activation='tanh', alpha=0.0001, batch_size='auto',
+                  beta_1=0.9, beta_2=0.999, early_stopping=True, epsilon=1e-06,
+                  hidden_layer_sizes=(100,), learning_rate='adaptive',
+                  learning_rate_init=0.001, max_iter=10000, momentum=0.9,
+                  n_iter_no_change=10, nesterovs_momentum=True, power_t=0.5,
+                  random_state=42, shuffle=True, solver='adam', tol=0.0001,
+                  validation_fraction=0.1, verbose=False, warm_start=False),
+    DecisionTreeClassifier(random_state=0),
+    ExtraTreesClassifier(n_jobs=-1),
+    GaussianNB()
+]
 
-    dev_vectors = []  # the avg-w2v for each sentence/review is stored in this list
-    for sent in tqdm(dev_tokens):  # for each tweet
-        sent_vec = np.zeros(n)  # as word vectors are of zero length 300,
-        cnt_words = 0  # num of words with a valid vector in the sentence/review
-        for word in sent:  # for each word in a tweet
-            if word in w2v_words:
-                vec = w2v_model.wv[word]
-                sent_vec += vec
-                cnt_words += 1
-        if cnt_words != 0:
-            sent_vec /= cnt_words
-        dev_vectors.append(sent_vec)
+# average Word2Vec
+print("average word2vec")
+# compute average word2vec for each tweet
+train_vectors = []  # the avg-w2v for each sentence/review is stored in this list
+for sent in tqdm(train_tokens):  # for each tweet
+    sent_vec = np.zeros(300)  # as word vectors are of zero length 300,
+    cnt_words = 0  # num of words with a valid vector in the sentence/review
+    for word in sent:  # for each word in a tweet
+        if word in w2v_words:
+            vec = w2v_model.wv[word]
+            sent_vec += vec
+            cnt_words += 1
+    if cnt_words != 0:
+        sent_vec /= cnt_words
+    train_vectors.append(sent_vec)
 
-    test_vectors = []  # the avg-w2v for each sentence/review is stored in this list
-    for sent in tqdm(test_tokens):  # for each tweet
-        sent_vec = np.zeros(n)  # as word vectors are of zero length 300,
-        cnt_words = 0  # num of words with a valid vector in the sentence/review
-        for word in sent:  # for each word in a tweet
-            if word in w2v_words:
-                vec = w2v_model.wv[word]
-                sent_vec += vec
-                cnt_words += 1
-        if cnt_words != 0:
-            sent_vec /= cnt_words
-        test_vectors.append(sent_vec)
+dev_vectors = []  # the avg-w2v for each sentence/review is stored in this list
+for sent in tqdm(dev_tokens):  # for each tweet
+    sent_vec = np.zeros(300)  # as word vectors are of zero length 300,
+    cnt_words = 0  # num of words with a valid vector in the sentence/review
+    for word in sent:  # for each word in a tweet
+        if word in w2v_words:
+            vec = w2v_model.wv[word]
+            sent_vec += vec
+            cnt_words += 1
+    if cnt_words != 0:
+        sent_vec /= cnt_words
+    dev_vectors.append(sent_vec)
 
-    for classifier in [
-        KNeighborsClassifier(n_neighbors=49),
-        LogisticRegression(n_jobs=-1),
-        LinearSVC(C=0.12, random_state=42),
-        MLPClassifier(activation='tanh', alpha=0.0001, batch_size='auto',
-                      beta_1=0.9, beta_2=0.999, early_stopping=True, epsilon=1e-06,
-                      hidden_layer_sizes=(100,), learning_rate='adaptive',
-                      learning_rate_init=0.001, max_iter=10000, momentum=0.9,
-                      n_iter_no_change=10, nesterovs_momentum=True, power_t=0.5,
-                      random_state=42, shuffle=True, solver='adam', tol=0.0001,
-                      validation_fraction=0.1, verbose=False, warm_start=False),
-        DecisionTreeClassifier(random_state=0),
-        ExtraTreesClassifier(n_jobs=-1),
-        GaussianNB()
-    ]:
-        clf = classifier.fit(train_vectors, train_labels)
-        predicted_labels = clf.predict(dev_vectors)
-        f1 = metrics.f1_score(
-            dev_labels, predicted_labels, average='micro')
-        print(f'{type(clf).__name__} F1 on N={n}： {f1:.4f}')
+test_vectors = []  # the avg-w2v for each sentence/review is stored in this list
+for sent in tqdm(test_tokens):  # for each tweet
+    sent_vec = np.zeros(300)  # as word vectors are of zero length 300,
+    cnt_words = 0  # num of words with a valid vector in the sentence/review
+    for word in sent:  # for each word in a tweet
+        if word in w2v_words:
+            vec = w2v_model.wv[word]
+            sent_vec += vec
+            cnt_words += 1
+    if cnt_words != 0:
+        sent_vec /= cnt_words
+    test_vectors.append(sent_vec)
+
+for classifier in classifiers:
+    clf = classifier.fit(train_vectors, train_labels)
+    predicted_labels = clf.predict(dev_vectors)
+    f1 = metrics.f1_score(
+        dev_labels, predicted_labels, average='micro')
+    print(f'{type(clf).__name__} F1： {f1:.4f}')
+
+print("l1 normalized sum word2vec")
+# compute l1-norm-sum word2vec for each tweet
+train_vectors = []  # the sum-w2v for each sentence/review is stored in this list
+for sent in tqdm(train_tokens):  # for each tweet
+    sent_vec = np.zeros(300)  # as word vectors are of zero length 300,
+    for word in sent:  # for each word in a tweet
+        if word in w2v_words:
+            vec = w2v_model.wv[word]
+            sent_vec += vec
+    train_vectors.append(sent_vec)
+# L1 normalize
+train_vectors = preprocessing.normalize(train_vectors, norm='l1')
+
+dev_vectors = []  # the avg-w2v for each sentence/review is stored in this list
+for sent in tqdm(dev_tokens):  # for each tweet
+    sent_vec = np.zeros(300)  # as word vectors are of zero length 300,
+    cnt_words = 0  # num of words with a valid vector in the sentence/review
+    for word in sent:  # for each word in a tweet
+        if word in w2v_words:
+            vec = w2v_model.wv[word]
+            sent_vec += vec
+            cnt_words += 1
+    if cnt_words != 0:
+        sent_vec /= cnt_words
+    dev_vectors.append(sent_vec)
+dev_vectors = preprocessing.normalize(dev_vectors, norm='l1')
+
+test_vectors = []  # the avg-w2v for each sentence/review is stored in this list
+for sent in tqdm(test_tokens):  # for each tweet
+    sent_vec = np.zeros(300)  # as word vectors are of zero length 300,
+    cnt_words = 0  # num of words with a valid vector in the sentence/review
+    for word in sent:  # for each word in a tweet
+        if word in w2v_words:
+            vec = w2v_model.wv[word]
+            sent_vec += vec
+            cnt_words += 1
+    if cnt_words != 0:
+        sent_vec /= cnt_words
+    test_vectors.append(sent_vec)
+dev_vectors = preprocessing.normalize(dev_vectors, norm='l1')
+
+for classifier in classifiers:
+    clf = classifier.fit(train_vectors, train_labels)
+    predicted_labels = clf.predict(dev_vectors)
+    f1 = metrics.f1_score(
+        dev_labels, predicted_labels, average='micro')
+    print(f'{type(clf).__name__} F1： {f1:.4f}')
+
+
+print("l2 normalized sum word2vec")
+# compute l1-norm-sum word2vec for each tweet
+train_vectors = []  # the sum-w2v for each sentence/review is stored in this list
+for sent in tqdm(train_tokens):  # for each tweet
+    sent_vec = np.zeros(300)  # as word vectors are of zero length 300,
+    for word in sent:  # for each word in a tweet
+        if word in w2v_words:
+            vec = w2v_model.wv[word]
+            sent_vec += vec
+    train_vectors.append(sent_vec)
+# L1 normalize
+train_vectors = preprocessing.normalize(train_vectors, norm='l2')
+
+dev_vectors = []  # the avg-w2v for each sentence/review is stored in this list
+for sent in tqdm(dev_tokens):  # for each tweet
+    sent_vec = np.zeros(300)  # as word vectors are of zero length 300,
+    cnt_words = 0  # num of words with a valid vector in the sentence/review
+    for word in sent:  # for each word in a tweet
+        if word in w2v_words:
+            vec = w2v_model.wv[word]
+            sent_vec += vec
+            cnt_words += 1
+    if cnt_words != 0:
+        sent_vec /= cnt_words
+    dev_vectors.append(sent_vec)
+dev_vectors = preprocessing.normalize(dev_vectors, norm='l2')
+
+test_vectors = []  # the avg-w2v for each sentence/review is stored in this list
+for sent in tqdm(test_tokens):  # for each tweet
+    sent_vec = np.zeros(300)  # as word vectors are of zero length 300,
+    cnt_words = 0  # num of words with a valid vector in the sentence/review
+    for word in sent:  # for each word in a tweet
+        if word in w2v_words:
+            vec = w2v_model.wv[word]
+            sent_vec += vec
+            cnt_words += 1
+    if cnt_words != 0:
+        sent_vec /= cnt_words
+    test_vectors.append(sent_vec)
+dev_vectors = preprocessing.normalize(dev_vectors, norm='l2')
+
+for classifier in classifiers:
+    clf = classifier.fit(train_vectors, train_labels)
+    predicted_labels = clf.predict(dev_vectors)
+    f1 = metrics.f1_score(
+        dev_labels, predicted_labels, average='micro')
+    print(f'{type(clf).__name__} F1： {f1:.4f}')
